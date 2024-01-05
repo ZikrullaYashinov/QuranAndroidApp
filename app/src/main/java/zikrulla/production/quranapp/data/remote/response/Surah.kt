0bdeeb4c8f57248244975051620e734c6cde8942
zikrulla <zikrullayashinov173@gmail.com>
2024-01-05 15:38:58 +0500

    add remote models @SerializedName

--- app/src/main/java/zikrulla/production/quranapp/data/remote/response/Surah.kt
+++ app/src/main/java/zikrulla/production/quranapp/data/remote/response/Surah.kt
@@ -1,14 +1,23 @@
 package zikrulla.production.quranapp.data.remote.response
 
+import com.google.gson.annotations.SerializedName
 import zikrulla.production.quranapp.data.model.Edition
 
 data class Surah(
+    @SerializedName("ayahs")
     var ayahs: List<Ayah>,
+    @SerializedName("edition")
     val edition: Edition,
+    @SerializedName("englishName")
     val englishName: String,
+    @SerializedName("englishNameTranslation")
     val englishNameTranslation: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("number")
     val number: Int,
+    @SerializedName("numberOfAyahs")
     val numberOfAyahs: Int,
+    @SerializedName("revelationType")
     val revelationType: String
 )
